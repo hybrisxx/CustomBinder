3d48566e9ff2949a9dd7c7fa35aea91b6771c96b
Hybris <jonas.kreienbuehl@gmail.com>
2025-07-09 15:01:00 +0200

    Add files via upload

---
+++ CustomBinder.lua
@@ -0,0 +1,88 @@
+-- Addon: CustomBinder
+
+-- Section Header in Key Bindings
+BINDING_HEADER_CUSTOMBINDER = "Custom Binder"
+
+-- Labels
+BINDING_NAME_CB_FIND_MINERALS       = "Find Minerals"
+BINDING_NAME_CB_CHARGER             = "Armored Black Deathcharger"
+BINDING_NAME_CB_HEARTHSTONE         = "Hearthstone"
+BINDING_NAME_CB_FISHING             = "Fishing"
+BINDING_NAME_CB_BLAZING_FORGE       = "Blazing Forge Kit"
+BINDING_NAME_CB_BRAINWASHING        = "Goblin Brainwashing Device"
+BINDING_NAME_CB_REPAIR_BOT          = "Field Repair Bot 75B"
+BINDING_NAME_CB_AUCTIONEER          = "Mechanical Auctioneer"
+BINDING_NAME_CB_PICK_LOCK           = "Pick Lock"
+BINDING_NAME_CB_FORWORN_MULE        = "Forworn Mule"
+BINDING_NAME_CB_FLOURISH            = "Flourish"
+BINDING_NAME_CB_MOLLE            = "MOLL-E Terminal"
+BINDING_NAME_CB_Battletank            = "Green Qiraji Battle Tank"
+BINDING_NAME_CB_Ghostly            = "Ghostly Strike"
+BINDING_NAME_CB_Kidney            = "Kidney Shot"
+BINDING_NAME_CB_Kick            = "Kick"
+
+-- Functions (global scope)
+
+function CB_FindMinerals()
+  CastSpellByName("Find Minerals")
+end
+
+function CB_ArmoredBlackDeathcharger()
+  CastSpellByName("Armored Black Deathcharger")
+end
+
+function CB_Hearthstone()
+  CastSpellByName("Hearthstone")
+end
+
+function CB_Fishing()
+  CastSpellByName("Fishing")
+end
+
+function CB_BlazingForgeKit()
+  CastSpellByName("Blazing Forge Kit")
+end
+
+function CB_GoblinBrainwashingDevice()
+  CastSpellByName("Goblin Brainwashing Device")
+end
+
+function CB_FieldRepairBot75B()
+  CastSpellByName("Field Repair Bot 75B")
+end
+
+function CB_MechanicalAuctioneer()
+  CastSpellByName("Mechanical Auctioneer")
+end
+
+function CB_PickLock()
+  CastSpellByName("Pick Lock")
+end
+
+function CB_ForwornMule()
+  CastSpellByName("Forworn Mule")
+end
+
+function CB_Flourish()
+  CastSpellByName("Flourish")
+end
+
+function CB_MOLLE()
+  CastSpellByName("MOLL-E Terminal")
+end
+
+function CB_BattleTank()
+  CastSpellByName("Green Qiraji Battle Tank")
+end
+
+function CB_Kick()
+  CastSpellByName("Kick")
+end
+
+function CB_Ghostly()
+  CastSpellByName("Ghostly Strike")
+end
+
+function CB_Kidney()
+  CastSpellByName("Kidney Shot")
+end
---
+++ Readme.txt
@@ -0,0 +1,8 @@
+CustomBinder allows you to assign specific spells directly to keybindings without using valuable action bar slots. 
+
+After installing the addon, you can bind keys to commonly used spells like "Hearthstone", "Fishing", and others directly through the Key Bindings menu under the Custom Binder section. 
+
+To add or change custom spells, you must edit both CustomBinder.lua and Bindings.xml:
+- Define a function in CustomBinder.lua using the exact spell name.
+- Add a corresponding <Binding> entry in Bindings.xml that calls that function.
+Ez
